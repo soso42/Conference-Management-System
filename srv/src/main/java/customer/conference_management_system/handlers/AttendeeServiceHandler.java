f493353eaa968c28581285d222b2e4b48cc0c9fe
Soso Janashvili <sosojanashvili@gmail.com>
2025-12-01 14:47:47 +0400

    added handler

--- srv/src/main/java/customer/conference_management_system/handlers/AttendeeServiceHandler.java
+++ srv/src/main/java/customer/conference_management_system/handlers/AttendeeServiceHandler.java
@@ -1,16 +1,30 @@
 package customer.conference_management_system.handlers;
 
-import cds.gen.Attendee;
+import cds.gen.TicketType;
+import cds.gen.Ticket_;
 import cds.gen.attendeeservice.Attendees_;
+import cds.gen.Attendee;
+import cds.gen.Ticket;
+import com.sap.cds.Result;
+import com.sap.cds.Struct;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
+import com.sap.cds.ql.Insert;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+
 @Component
-@ServiceName("AttendeeService")
+@ServiceName({ "AttendeeService" })
+@RequiredArgsConstructor
 public class AttendeeServiceHandler implements EventHandler {
+
+    private final PersistenceService persistenceService;
+
 
     @Before(
             event = CqnService.EVENT_CREATE,
@@ -18,6 +32,18 @@ public class AttendeeServiceHandler implements EventHandler {
     )
     public void beforeCreation(Attendee attendee) {
 
+        Ticket newTicket = Struct.create(Ticket.class);
+        newTicket.setType(TicketType.REGULAR);
+        newTicket.setPrice(123);
+        newTicket.setConference(Map.of("ID", "22cbcaaa-af0c-4ecb-affb-27f487e85674"));
+
+        // Persist the ticket directly using PersistenceService
+        Result result = persistenceService.run(
+                Insert.into(Ticket_.CDS_NAME).entry(newTicket)
+        );
+        Ticket savedTicket = result.first().get().as(Ticket.class);
+
+        attendee.setTicket(savedTicket);
     }
 
 }
